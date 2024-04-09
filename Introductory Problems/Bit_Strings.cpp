a053820f754cc4cb8eb748f44825f73c0672dd2a
Pushkar Narkhede <pushkarkn15@gmail.com>
2024-04-09 13:38:18 +0530

    created Bit_Strings.cpp

--- Introductory Problems/Bit_Strings.cpp
+++ Introductory Problems/Bit_Strings.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 #define ll long long
+const ll MOD = (ll) 1e9 + 7;
 
 ll binpow(ll a, ll x) {
     ll res = 1;
     while (x > 0) {
         if (x & 1)
-            res = res * a;
-        a = a * a;
+            res = res * a % MOD;
+        a = a * a % MOD;
         x >>= 1;
     }
     return res;
@@ -17,7 +18,7 @@ ll binpow(ll a, ll x) {
 int main() {
     ll n; 
     cin >> n;
-    
+
     /* compute 2^n using Binary Exponentiation Algorithm*/
     cout << binpow(2LL, n);
 }
