3b564747c70c578b95cf247c682ca101a3a6509a
Pushkar Narkhede <pushkarkn15@gmail.com>
2024-04-16 18:49:59 +0530

    created Counting_Divisors.cpp

---
+++ Mathematics/Counting_Divisors.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int MOD = 1e9+7;
+
+
+/* bruteforce O(sqrt(x)) */
+
+
+void solve(){
+    int x;
+    cin >> x;
+
+    int count = 0;
+
+    for (int i = 1; i * i <= x; i++) {
+        if (x % i == 0) {
+            count += 2;
+        }
+        if(i*i == x) count--;
+    }
+
+    cout << count << endl;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    while(n--){
+        solve();
+    }
+}
