a75b51512601e69617a555d95061305323f7e190
Pushkar Narkhede <pushkarkn15@gmail.com>
2024-04-17 19:20:25 +0530

    created Sum_of_Two_Values.cpp

---
+++ Sorting and Searching/Sum_of_Two_Values.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int MOD = 1e9+7;
+
+/* 
+    Two Sum approach 
+    Maintain a hashmap to chck for existing complement
+*/
+
+int main() {
+    int n, target;
+    cin >> n >> target;
+
+    vector<int> a(n);
+    map<int,int> mp;
+
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+        int complement = target - a[i];
+
+        if(mp.find(complement) != mp.end()){
+            cout << i + 1 << " " << mp[complement] + 1;
+            return 0;
+        }
+
+        mp[a[i]] = i;
+    }
+
+    cout << "IMPOSSIBLE";
+}
